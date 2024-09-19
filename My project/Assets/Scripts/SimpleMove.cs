35c8ef5eaec6c95696ed9ac38f8c49a71e59121a
Giuliano Gonzales <92641300+giugonzales07@users.noreply.github.com>
2024-09-18 21:27:40 -0300

    Script Rotate

---
+++ My project/Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update(){
+        // (x, y, z)
+        // (frente/trás, cima/baixo, esquerdaa/direita)
+        transform.Translate(2f * Time.deltaTime, -0.5f * Time.deltaTime, 0f);
+    }
+}
---
+++ My project/Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update(){
+        // (x, y, z)
+        // (frente/trás, cima/baixo, esquerdaa/direita)
+        transform.Rotate(0f, 5f * Time.deltaTime, 0f);
+        //transform.Translate(2f * Time.deltaTime, -0.5f * Time.deltaTime, 0f);
+    }
+}
