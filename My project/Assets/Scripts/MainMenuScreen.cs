b85beb8a72720ca9aa4028120c3d21bda48c9c50
Giuliano Gonzales <92641300+giugonzales07@users.noreply.github.com>
2024-12-11 19:06:55 -0300

    Main Menu

---
+++ My project/Assets/Scripts/MainMenuScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void GoToInstructionMenu(){
+        SceneManager.LoadScene("InstructionMenu");
+    }
+
+    public void GoToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitButton(){
+        FindObjectOfType<GameManager>().ExitGame();
+    }
+}
